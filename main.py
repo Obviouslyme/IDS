e843bcaee602335008d27c1452d4dab385ede8db
Juliette <juliette.cantais@hotmail.com>
2024-04-14 18:23:32 -0400

    add dataset

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from sklearn.metrics import classification_report,confusion_matrix,accuracy_scor
 from sklearn.metrics import f1_score
 from sklearn.tree import DecisionTreeClassifier
 
-df = pd.read_csv('./data/CICIDS2017_sample.csv', delimiter=',')
+df = pd.read_csv('./data/dataset.csv', delimiter=',')
 
 numeric_features = df.dtypes[df.dtypes != 'object'].index
 df[numeric_features] = df[numeric_features].apply(
@@ -16,35 +16,37 @@ df[numeric_features] = df[numeric_features].apply(
 # Fill empty values by 0
 df = df.fillna(0)
 
-labelencoder = LabelEncoder()
-df.iloc[:, -1] = labelencoder.fit_transform(df.iloc[:, -1:])
-x_data = df.drop(['Label'],axis=1).values.astype('int')
-y_data = df.iloc[:, -1].values.reshape(-1,1)
-y_data=np.ravel(y_data).astype('int')
-X_train, X_test, y_train, y_test = train_test_split(x_data, y_data, train_size = 0.8, test_size = 0.2, random_state = 0,stratify = y_data)
+print(df.Label.value_counts())
 
-X_train.shape
+# labelencoder = LabelEncoder()
+# df.iloc[:, -1] = labelencoder.fit_transform(df.iloc[:, -1:])
+# x_data = df.drop(['Label'],axis=1).values.astype('int')
+# y_data = df.iloc[:, -1].values.reshape(-1,1)
+# y_data=np.ravel(y_data).astype('int')
+# X_train, X_test, y_train, y_test = train_test_split(x_data, y_data, train_size = 0.8, test_size = 0.2, random_state = 0,stratify = y_data)
 
-from imblearn.over_sampling import SMOTE
-smote=SMOTE(n_jobs=-1,sampling_strategy={4:1500}) # Create 1500 samples for the minority class "4"
+# X_train.shape
 
-X_train, y_train = smote.fit_resample(X_train, y_train)
+# from imblearn.over_sampling import SMOTE
+# smote=SMOTE(n_jobs=-1,sampling_strategy={4:1500}) # Create 1500 samples for the minority class "4"
 
-# Decision tree training and prediction
-dt = DecisionTreeClassifier(random_state = 0)
-dt.fit(X_train,y_train)
-dt_score=dt.score(X_test,y_test)
-y_predict=dt.predict(X_test)
-y_true=y_test
-print('Accuracy of DT: '+ str(dt_score))
-precision,recall,fscore,none= precision_recall_fscore_support(y_true, y_predict, average='weighted') 
-print('Precision of DT: '+(str(precision)))
-print('Recall of DT: '+(str(recall)))
-print('F1-score of DT: '+(str(fscore)))
-print(classification_report(y_true,y_predict))
-cm=confusion_matrix(y_true,y_predict)
-f,ax=plt.subplots(figsize=(5,5))
-sns.heatmap(cm,annot=True,linewidth=0.5,linecolor="red",fmt=".0f",ax=ax)
-plt.xlabel("y_pred")
-plt.ylabel("y_true")
-plt.show()
+# X_train, y_train = smote.fit_resample(X_train, y_train)
+
+# # Decision tree training and prediction
+# dt = DecisionTreeClassifier(random_state = 0)
+# dt.fit(X_train,y_train)
+# dt_score=dt.score(X_test,y_test)
+# y_predict=dt.predict(X_test)
+# y_true=y_test
+# print('Accuracy of DT: '+ str(dt_score))
+# precision,recall,fscore,none= precision_recall_fscore_support(y_true, y_predict, average='weighted') 
+# print('Precision of DT: '+(str(precision)))
+# print('Recall of DT: '+(str(recall)))
+# print('F1-score of DT: '+(str(fscore)))
+# print(classification_report(y_true,y_predict))
+# cm=confusion_matrix(y_true,y_predict)
+# f,ax=plt.subplots(figsize=(5,5))
+# sns.heatmap(cm,annot=True,linewidth=0.5,linecolor="red",fmt=".0f",ax=ax)
+# plt.xlabel("y_pred")
+# plt.ylabel("y_true")
+# plt.show()
